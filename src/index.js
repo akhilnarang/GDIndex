c7d566b55b8606dfaa2ced8f94b6111ab7c82fef
Yash Garg <yash.garg@outlook.in>
2021-03-22 01:37:29 +0530

    index: use viewport meta tag

--- src/index.js
+++ src/index.js
@@ -118,6 +118,7 @@ async function handleRequest(request) {
 <html>
 <head>
 <title>${title}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
